61bfd307bb123f8fa6fa1478ac893b29fdfbbdc8
Pallav Kumar Sharma <35151342+2016uar1152@users.noreply.github.com>
2021-10-01 12:15:00 +0530

    UI initiated, completion required

--- BatchDataProcessing/src/main/java/com/training/queue/ActiveMqConsumer.java
+++ BatchDataProcessing/src/main/java/com/training/queue/ActiveMqConsumer.java
@@ -1,5 +1,6 @@
 package com.training.queue;
 
+import java.lang.annotation.Retention;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,45 +9,30 @@ import org.springframework.stereotype.Component;
 
 import com.training.bean.FailedProduct;
 
-//@Component
+@Component
 public class ActiveMqConsumer {
-    
+	
 	public static List<FailedProduct> list = new ArrayList<FailedProduct>();
 	
-	//@JmsListener(destination = "ProductQueue")
+	@JmsListener(destination = "ProductQueue" )
 	public void listener(FailedProduct failedProduct){
 		list.add(failedProduct);
 	}
 	
 	public void processFailedProducts(List<FailedProduct> list){
+		String[] messageArray = {"", "UPC can't be empty!!", "Code for Artist ID cannot be empty or have more than 7 digits.",
+				"Code for Organisation ID cannot be empty or more than 8 digits.","Release Date format is incorrect. Only YYYYMMDD allowed.",
+				"Release Date must have 8 digits."};
 		for(FailedProduct failedProduct: list) {
-			int messageId = failedProduct.getMessageId();
-			String message="";
-			if(messageId==1)
-			{
-				message="UPC can't be empty!!";
-			}
-			else if(messageId==2)
-			{
-				message="Code for Artist ID cannot be empty or have more than 7 digits.";
-			}
-			else if(messageId==3)
-			{
-				message="Code for Organisation ID cannot be empty or more than 8 digits.";
-			}
-			else if(messageId==4)
-			{
-				message="Release Date format is incorrect. Only YYYYMMDD allowed.";
-			}
-			else if(messageId==5)
-			{
-				message="Release Date must have 8 digits.";
-			}
+			String message=messageArray[failedProduct.getMessageId()];
+			
 			//send to ui for modification with message
 			//after modification reprocess the failed product
 		}
 		
 	}
+	
+	
 	
 	
 }
